5db45c58a70e3a70c1c2a4cd0647c853264645c7
sonichk01 <157278002+sonichk01@users.noreply.github.com>
2024-01-21 11:37:13 +0200

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,169 +1,134 @@
-from pygame import *
-from random import randint
- 
-# фонова музика
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
-fire_sound = mixer.Sound('fire.ogg')
- 
-# шрифти і написи
-font.init()
-font1 = font.Font(None, 80)
-win = font1.render('YOU WIN!', True, (255, 255, 255))
-lose = font1.render('YOU LOSE!', True, (180, 0, 0))
-font2 = font.Font(None, 36)
+from typing import Any 
+from pygame import * 
+from random import randint   
 
-
-# нам потрібні такі картинки:
-img_back = "galaxy.jpg"  # фон гри
-img_hero = "tank.png"  # герой
-img_bullet = "bullet.png" # куля
-img_enemy = "ufo.png"  # ворог
+mixer.init()  
+mixer.music.load("space.ogg")  
+mixer.music.play()  
+fire_sound = mixer.Sound('fire.ogg') 
  
-score = 0  # збито кораблів
-goal = 30 # стільки кораблів потрібно збити для перемоги
-lost = 0  # пропущено кораблів
-max_lost = 5 # програли, якщо пропустили стільки
+font.init() 
+font1 = font.Font(None, 100) 
+win  = font1.render('YOU WIN!', True, (255, 255, 255))     
 
+font2 = font.Font(None, 50) 
+ 
+img_back = "fon.jpg" 
+img_hero = "mario.webp" 
+img_enemy = "red.png" 
+img_bullet = "kvi.webp" 
+img_enemy2 = "mone.png"
+ 
+goal = 15 
+score = 0 
+lost = 0 
+max_lost = 5 
+ 
+class GameSprite(sprite.Sprite): 
+    def __init__(self, player_imege, player_x, player_y, size_x, size_y, player_speed): 
+        sprite.Sprite.__init__(self) 
+        self.image = transform.scale(image.load(player_imege), (size_x, size_y)) 
+        self.speed = player_speed 
+        self.rect = self.image.get_rect() 
+        self.rect.x = player_x 
+        self.rect.y = player_y 
+ 
+    def reset(self):  
+        window.blit(self.image, (self.rect.x, self.rect.y)) 
+ 
+class Player(GameSprite): 
+    def update(self): 
+        keys = key.get_pressed() 
+        if keys[K_LEFT] and self.rect.x > 5: 
+            self.rect.x -= self.speed 
+        if keys[K_RIGHT] and self.rect.x < win_width - 80: 
+            self.rect.x += self.speed 
+ 
+    def fire(self): 
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15) 
+        bullets.add(bullet) 
+ 
+class Enemy(GameSprite): 
+    def update(self): 
+        self.rect.y += self.speed 
+        global lost  
+        if self.rect.y > win_height: 
+            self.rect.x = randint(80, win_width -80) 
+            self.rect.y = 0 
+            lost += 1 
+ 
+class Bullet(GameSprite): 
+    def update(self): 
+        self.rect.y += self.speed 
+        if self.rect.y < 0: 
+            self.kill() 
+ 
+win_width = 700  
+win_height = 500  
+display.set_caption("Shooter")  
+window = display.set_mode((win_width, win_height))  
+background = transform.scale(image.load(img_back), (win_width, win_height))  
+ 
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10) 
+monsters = sprite.Group() 
+for i in range(1, 6): 
+    monster = Enemy(img_enemy, randint(80, win_width -80), -40, 80, 50, randint(1, 5)) 
+    monster = Enemy(img_enemy2, randint(80, win_width -80), -40, 80, 50, randint(1, 5)) 
+    monsters.add(monster) 
+ 
+bullets = sprite.Group() 
+ 
+finish = False 
+ 
+run = True  
+ 
+while run: 
+    for e in event.get(): 
+        if e.type == QUIT: 
+            run = False 
+ 
+        elif e.type == KEYDOWN: 
+            if e.key == K_SPACE: 
+                fire_sound.play() 
+                ship.fire() 
+ 
+    if not finish: 
+        window.blit(background, (0, 0)) 
+        text = font2.render("Рахунок: " + str(score), 4, (255, 255, 255)) 
+        window.blit(text, (10, 20)) 
+ 
+        text2 = font2.render("Монети: " + str(lost), 1, (255, 255, 255)) 
+        window.blit(text2, (10, 50)) 
+ 
+        ship.update() 
+        monsters.update() 
+        bullets.update() 
+  
+        ship.reset() 
+        monsters.draw(window) 
+        bullets.draw(window) 
+ 
+        collides = sprite.groupcollide(monsters, bullets, True, True) 
+        for c in collides: 
+            score = score + 1 
+            monster = Enemy(img_enemy, randint(80, win_width -80), -40, 80, 50, randint(1, 5)) 
+            monsters.add(monster) 
 
-# клас-батько для інших спрайтів
-class GameSprite(sprite.Sprite):
-    # конструктор класу
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # викликаємо конструктор класу (Sprite):
-        sprite.Sprite.__init__(self)
- 
-        # кожен спрайт повинен зберігати властивість image - зображення
-        self.image = transform.scale(
-            image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
- 
-        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
- 
-    # метод, що малює героя у вікні
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
- 
-# клас головного гравця
-class Player(GameSprite):
-    # метод для керування спрайтом стрілками клавіатури
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
- 
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
- 
-# клас спрайта-ворога
-class Enemy(GameSprite):
-    # рух ворога
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        # зникає, якщо дійде до краю екрана
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
-
-
-# клас спрайта-кулі  
-class Bullet(GameSprite):
-    # рух ворога
-    def update(self):
-        self.rect.y += self.speed
-        # зникає, якщо дійде до краю екрана
-        if self.rect.y < 0:
-            self.kill()
-
-
-# створюємо віконце
-win_width = 700
-win_height = 500
-display.set_caption("Shooter")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
- 
-# створюємо спрайти
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
- 
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(
-        80, win_width - 80), -40, 80, 50, randint(1, 5))
-    monsters.add(monster)
- 
-bullets = sprite.Group()
-
-
-# змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
-finish = False
-# Основний цикл гри:
-run = True  # прапорець скидається кнопкою закриття вікна
- 
-while run:
-    # подія натискання на кнопку Закрити
-    for e in event.get():
-        if e.type == QUIT:
-            run = False
-        #подія натискання на пробіл - спрайт стріляє
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                fire_sound.play()
-                ship.fire()
-   
-    # сама гра: дії спрайтів, перевірка правил гри, перемальовка
-    if not finish:
-        # оновлюємо фон
-        window.blit(background, (0, 0))
- 
-        # пишемо текст на екрані
-        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
- 
-        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
-        # рухи спрайтів
-        ship.update()
-        monsters.update()
-        bullets.update()
-
-
-        # оновлюємо їх у новому місці при кожній ітерації циклу
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
-
-
-        # перевірка зіткнення кулі та монстрів (і монстр, і куля при зіткненні зникають)
-        collides = sprite.groupcollide(monsters, bullets, True, True)
-        for c in collides:
-            # цей цикл повториться стільки разів, скільки монстрів збито
+        collides = sprite.groupcollide(monsters, bullets, True, True) 
+        for c in collides: 
             score = score + 1
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            monsters.add(monster)
-
-
-        # можливий програш: пропустили занадто багато або герой зіткнувся з ворогом
-        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
-            finish = True # програли, ставимо тло і більше не керуємо спрайтами.
-            window.blit(lose, (200, 200))
-           
-        # перевірка виграшу: скільки очок набрали?
-        if score >= goal:
-            finish = True
-            window.blit(win, (200, 200))
-
-
-        display.update()
-    # цикл спрацьовує кожні 0.05 секунд
-    time.delay(50)
+            monster = Enemy(img_enemy2, randint(80, win_width -80), -40, 80, 50, randint(1, 5)) 
+            monsters.add(monster) 
+ 
+        if sprite.spritecollide(ship, monsters, True) or lost >= max_lost: 
+            finish =False
+            
+            
+         
+        if score >= goal: 
+            finish = False
+            window.blit(win, (200, 200)) 
+ 
+        display.update() 
+ 
+    time.delay(40)
